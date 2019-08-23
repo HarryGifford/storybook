a982c91ca83c12f5a228a028e1e63ff67fe30ce3
Harry Gifford <harry@gifford.eu>
2019-08-23 10:51:25 -0700

    Update Syntax highlighter version

--- lib/components/src/typings.d.ts
+++ lib/components/src/typings.d.ts
@@ -2,3 +2,28 @@ declare module 'global';
 declare module 'simplebar-react';
 declare module 'markdown-to-jsx';
 declare module '*.md';
+
+declare module 'react-syntax-highlighter/dist/cjs/languages/prism/jsx' {
+    const language: any;
+    export default language;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/languages/prism/bash' {
+    const language: any;
+    export default language;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/languages/prism/css' {
+    const language: any;
+    export default language;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/languages/prism/markup' {
+    const language: any;
+    export default language;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/prism-light' {
+    const light: any;
+    export default light;
+}
